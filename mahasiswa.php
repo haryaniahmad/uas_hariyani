440526721203361ea6150e1be33885d75f9d20aa
haryaniahmad <137623327+haryaniahmad@users.noreply.github.com>
2023-06-24 20:55:40 +0700

    Add files via upload

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,60 +1,19 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <title></title>
-  </head>
-  <body>
- 
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-12">
- 
-                <?php if(!empty(session()->getFlashdata('message'))) : ?>
- 
-                <div class="alert alert-success">
-                    <?php echo session()->getFlashdata('message');?>
-                </div>
-                    
-                <?php endif ?>
- 
-               
-                <a href="<?php echo base_url('mahasiswa/input/') ?>" class="btn btn-md btn-success mb-3">TAMBAH DATA</a>
-                <table class="table table-bordered table-striped">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th>JK</th>
-                            <th>JUMLAH</th>
-                               <th>MATA KULIAH</th>
-                
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach($mahasiswa as $key => $anis) : ?>
- 
-                            <tr>
-                                <td><?php echo $anis['jk'] ?></td>
-                                <td><?php echo $anis['jumlah'] ?></td>
-                                  <td><?php echo $anis['matkul'] ?></td>
-                              
-                            </tr>
- 
-                        <?php endforeach ?>
-                    </tbody>
-                </table>
-              
-            </div>
-        </div>
-    </div>
- 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php
+    namespace App\Controllers;
+    use App\Models\Modelmahasiswa;
+
+    class mahasiswa extends BaseController
+    {
+        function __construct (){
+            $this->Modelmahasiswa = new Modelmahasiswa();
+            //$this ->typemahasiswa = new Modelmahasiswa();
+        }
+        public function index()
+        {
+            $data['mahasiswa']= $this->Modelmahasiswa->getmahasiswa();
+
+            return view('mahasiswa', $data);
+        }
+      
+    }
+?>
